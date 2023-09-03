3b3412051b2cc8699f3a68752f43ad59281d6004
zphtet <zinphtet.215108@gmail.com>
2023-09-03 23:56:49 +0630

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,26 @@ export default {
     fontFamily: {
       sans: ["Nunito Sans", "sans-serif"],
     },
+    screens: {
+      pc: { max: "1200px" },
+      // => @media (max-width: 1535px) { ... }
+      tb: { max: "768px" },
+      // => @media (max-width: 1279px) { ... }
+
+      ml: { max: "425px" },
+      // => @media (max-width: 1023px) { ... }
+
+      mm: { max: "375px" },
+      // => @media (max-width: 767px) { ... }
+
+      ms: { max: "320px" },
+      // => @media (max-width: 639px) { ... }
+    },
 
     extend: {
+      maxWidth: {
+        container: "87.5rem",
+      },
       colors: {
         dblue: "hsl(209, 23%, 22%)",
         "dblue-bg": "hsl(207, 26%, 17%)",
